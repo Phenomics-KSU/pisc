3998be1f64ced06c9594a38b4f1db19f6ffeb970
Kyle McGahee <kmcgahee@ksu.edu>
2015-06-30 22:57:04 -0500

    Reworked canon driver (and MCU code) since sensor thread wasn't getting ran at a consistent rate (windows/linux aren't real-time OSs).

--- pisc/sensors/canon_mcu.py
+++ pisc/sensors/canon_mcu.py
@@ -4,8 +4,8 @@
 Sensor Name:    Canon EOS Camera (tested with T5i and 7D)
 Manufacturer:   Canon
 Sensor Type:    Camera
-Modifications:  Equipped with Arduino mini pro microcontroller with USB shield using PTP library.
-NOTES:          Due to timing constraints the 1st (and sometimes 2nd) captured image doesn't get logged. 
+Modifications:  Equipped with Arduino mini pro microcontroller (MCU) with USB shield using PTP library.
+Notes:          Due to how the PTP library and MCU are setup the very last captured image isn't logged. 
 """
 
 import time
@@ -31,10 +31,27 @@ class CanonMCU(Sensor):
         self.stop_triggering = False # If true then will stop taking pictures.
         self.connection = None
         
-        # Command to send to MCU to trigger camera. Send many trigger commands to minimize chance one gets missed.
-        self.trigger_command = '\x61' * 20
+        # Command to send to MCU to sync camera time. 
+        self.sync_command = '\x73' # ascii s
+        
+        # Command to send to MCU to trigger camera. 
+        self.trigger_command = '\x74' # ascii t
+        
+        # Acknowledgment command
+        self.ack_command = '\x61' # ascii a
+        
+        # UTC timestamp that all MCU reported image times are relative to.
+        self.synced_utc_time = 0
+        
+        # Last image filename received by camera.  
+        self.last_image_filename = 'none'
+        
+        # How many dump messages that filename couldn't be extracted from.
+        self.failed_image_name_parse_count = 0
         
         self.image_count = 0
+        
+        self.input_stream = '' # data buffer received from MCU that hasn't been used yet
         
     def open(self):
         '''Open serial port.'''
@@ -56,72 +73,178 @@ class CanonMCU(Sensor):
         self.connection.flushInput()
         self.connection.flushOutput()
         
-        # Pause for two seconds before sending any commands to fix timing issue.
+        # Pause for two seconds before sending any commands to give MCU time to startup and fix weird timing issue.
         time.sleep(2)
         
+        # Wait until have a valid time source before starting camera.
+        current_time = self.time_source.time    
+        while current_time == 0:
+            time.sleep(0.25)
+            current_time = self.time_source.time
+        
+        # Create mapping between our time source and relative MCU time source so we can tag images with times.
+        self.sync_mcu_time()
+            
         while True:
             
             if self.stop_triggering:
                 # Don't want to take pictures right now.
+                self.disable_periodic_triggering()
                 time.sleep(0.5)
                 continue
-            
-            if self.trigger_period <= 0:
-                # Suspend thread until we receive a trigger command.
-                # TODO block
-                raise NotImplementedError
-            
-            # Grab time before sending trigger command to MCU since image should capture immediately after.
-            time_of_reading = self.time_source.time
-            
-            if time_of_reading == 0:
-                # Haven't received a valid time yet so don't take any images.
-                time.sleep(0.25)
-                continue
-            
-            # Tell microcontroller to tell camera to take picture. 
-            self.connection.write(self.trigger_command)
 
-            logging.getLogger().debug('Sending trigger command.')
+            # Tell camera how often we want to take pictures.  Do it each time through loop in case MCU doesn't pick up on one command.
+            # Convert to an integer in milliseconds because that's what MCU is expecting.
+            self.change_trigger_period(int(self.trigger_period * 1000))
             
-            self.image_count += 1
+            # Try to read in any new sensor data.  Set timeout so give camera time to respond, but can also warn user that no data is coming back.
+            self.connection.timeout = self.trigger_period + 2
+            newly_read_data = self.connection.read()
+            
+            if newly_read_data is None or len(newly_read_data) == 0:
+                logging.getLogger().warning('No new data received from camera {}. Is it still plugged in?'.format(self.sensor_name))
+                continue
+            
+            logging.getLogger().debug('Camera {} read in {} bytes'.format(self.sensor_name, len(newly_read_data)))
+            #logging.getLogger().debug(newly_read_data)
+            
+            # Try to parse image filenames and relative time stamps out of the new data.
+            messages = self.parse_new_data(newly_read_data)
+            new_images = self.handle_new_messages(messages)
 
-            # Suspend execution until we want to trigger again. 
-            # Normally this would happen at the end of the loop, but we need to wait for the image file name to get sent back.
-            if self.image_count <= 1:
-                # On first image we need to at least wait a couple seconds for camera to initialize and to receive large event dump.
-                sleep_time = max(2, self.trigger_period)
-            else:
-                sleep_time = self.trigger_period
-                
-            time.sleep(sleep_time)
-            
-            # Read all available bytes from serial port.  This won't block.
-            bytes_to_read = self.connection.inWaiting()
-            raw_event_dump = self.connection.read(bytes_to_read)
-            
-            logging.getLogger().debug('Read in {0} bytes'.format(len(raw_event_dump)))
-            #logging.getLogger().debug(raw_event_dump)
-            
-            if len(raw_event_dump) == 0:
-                logging.getLogger().warning('Camera: {0} no data returned from MCU.'.format(self.sensor_name))
-                continue
-            
-            if self.image_count <= 1:
-                # Camera won't return filename until NEXT time we time a picture (lags 1 behind).
-                continue
-            
-            # Try to parse a filename out of the camera event dump.
-            image_filename = self.parse_filename(raw_event_dump)
-            if len(image_filename) == 0:
-                logging.getLogger().warning('Camera: {0} could not parse filename from event dump.'.format(self.sensor_name))
-                # statuses = parse_status_message(raw_event_dump)
-                continue
-            
-            logging.getLogger().debug(image_filename)
-            
-            self.handle_data((time_of_reading, image_filename))
+            for (image_utc_time, filename) in new_images:
+                self.handle_data((image_utc_time, filename))
         
+    def sync_mcu_time(self):
+        '''
+         Synchronize camera MCU to utc time.  This won't start taking pictures, but will tell the camera MCU
+         to report all future image times relative to when it receives this 'sync' command.  
+         This is better than have this driver send trigger commands since there's no guarantee this code
+         can run at a constant rate since the OS isn't real-time.
+         
+         Notes: Time source must have a valid non-zero time before calling this method.
+        '''
+        sync_successful = False
+        while not sync_successful:
+            # Grab current UTC time so we can use relative MCU times when images come back.
+            self.synced_utc_time = self.time_source.time
+            sync_successful = self.send_command(self.sync_command, command_description='sync', ack_timeout=1)
+            
+    def change_trigger_period(self, new_trigger_period):
+        '''Change how often camera is taking pictures.  Should be in milliseconds.  Set to zero to stop taking images.'''
+        self.send_command('p{}\n'.format(new_trigger_period), 'change trigger period', ack_timeout=0)
+        
+    def disable_periodic_triggering(self):
+        '''Tell MCU to stop triggering camera at specified rate.'''
+        self.change_trigger_period(0)
+        
+    def send_command(self, command, command_description, ack_timeout):
+        '''
+        Send specified command over connection.  Command description should describe what type of command is being sent. 
+        Ack timeout is the serial port read timeout specified in seconds.  If not ack is expected then set timeout argument 
+        to zero.  Return true if method is successful.
+        '''
+        if self.connection is None:
+            logging.getLogger().error('Could not send {} command to camera {} due to serial port not being open.'.format(command_description, self.sensor_name))
+            return False
+        
+        self.connection.write(command)
+        
+        if ack_timeout <= 0:
+            return True # don't want to wait for acknowledgment.
+        
+        # update read timeout on serial port
+        self.connection.timeout = ack_timeout
+        
+        ack_response = self.connection.read()
+        
+        if ack_response is None or len(ack_response) == 0:
+            logging.getLogger().error('Timed out when waiting for acknowledgment from camera {} to {} command.'.format(self.sensor_name, command_description))
+            return False # no ack received
+        
+        if ack_response == self.ack_command:
+            return True # received correct ack command
+        else:
+            logging.getLogger().error('Received \'{}\' from camera {} when waiting for ack command \'{}\'.'.format(ack_response, self.sensor_name, self.ack_commands))
+            return False
+        
+    def parse_new_data(self, data):
+        '''Return list of new images where an image is (utc timestamp, filename)''' 
+        # Add data onto running list of data that hasn't been used.
+        self.input_stream += data
+        
+        messages = []
+        while True:
+            new_message = self.extract_message_from_input_stream()
+            if new_message is None:
+                break # no more message right now
+            messages.append(new_message)
+        
+        return messages
+                
+    def handle_new_messages(self, messages):
+        '''Return list of new images where an image is a (utc_time, filename). Also logs any status messages.'''
+        new_images = []
+        
+        for (message_type, contents) in messages:
+        
+            if message_type == 'status':
+                logging.getLogger().warning('Camera {} reported status {}'.format(self.sensor_name, contents))
+            elif message_type == 'dump':
+                filename = self.parse_filename(contents)
+                if filename is not None and len(filename) > 0:
+                    self.last_image_filename = filename
+                else:
+                    self.failed_image_name_parse_count += 1
+                    if self.image_count > 1:
+                        # We've already parsed one successful image so we shouldn't have a failure here.
+                        logging.getLogger().error('Could not parse filename from camera {} after successfully parsing one.'.format(self.sensor_name))
+                    elif self.failed_image_name_parse_count > 3:
+                        # Check how many we've failed to parse.  If it's too many than the user problem specified the wrong camera prefix.
+                        logging.getLogger().error('Failed to parse filename from camera {} multiple times.  Is specified image prefix {} correct?'.format(self.sensor_name, self.image_filename_prefix))
+            elif message_type == 'time':
+                try:
+                    relative_time = float(contents) / 1000.0 # convert from milliseconds to seconds
+                except ValueError:
+                    logging.getLogger().error('Camera {} MCU time could not be converted to a float.'.format(self.sensor_name))
+                    continue # invalid time
+                utc_time = self.synced_utc_time + relative_time  
+                new_images.append((utc_time, filename))
+                self.image_count += 1
+
+        return new_images
+    
+    def extract_message_from_input_stream(self):
+        '''
+        Return first message from input stream where a message is a type (ie time) followed by contents.
+        Once a message is parsed, it and anything before it is deleted from input stream.
+        '''
+        message_start = '<*<'
+        message_end = '>*>'
+        message_start_index = self.input_stream.find(message_start)
+        if message_start_index < 0:
+            return None
+        message_end_index = self.input_stream.find(message_end)
+        if message_end_index < 0:
+            return None
+        
+        message = self.input_stream[message_start_index + len(message_start) : message_end_index]
+
+        type_end_index = message.find('-')
+        
+        if type_end_index < 0:
+            message_type = 'unknown'
+        else:
+            message_type = message[:type_end_index].lower()
+            
+        message_contents = message[type_end_index+1:]
+
+        # Clear message from input buffer now that we've extracted it.
+        very_end_index = message_end_index + len(message_end)
+        self.input_stream = self.input_stream[very_end_index:]
+
+        return (message_type, message_contents)
+
     def parse_filename(self, rawdata):
         '''Extract filename from raw event dump from camera.'''
         # Find index of filename prefix.
--- pisc/sensors/canon_mcu_code/canon_eos_trigger.ino
+++ pisc/sensors/canon_mcu_code/canon_eos_trigger.ino
@@ -9,7 +9,8 @@
 void reportInWrongState(char const * state);
 
 // Status frame starts and stop strings that report status messages back to client.
-char const * const statusFrame = "<<%>>";
+char const * const statusStartFrame = "<*<";
+char const * const statusEndFrame = ">*>";
 
 class CamStateHandlers : public EOSStateHandlers
 {
@@ -43,7 +44,11 @@ static CanonEOS            eos(&usb, &camStates);
 static byte inByte = 0; // Byte read in from serial port.
 static bool receivedTriggerCommand = false;
 static bool usbInitializedCorrectly = true;
-static uint32_t last_capture_attempt = 0;
+static bool synced_to_client = false; // becomes true when first receive sync command from client.
+static uint32_t sync_time = 0; // MCU time that the sync command was received.  In milliseconds.
+static uint32_t trigger_period = 0; // Period in milliseconds before triggering camera.  If set to 0 then periodic triggering is disabled.
+static uint32_t previous_capture_time = 0; // Time in milliseconds of the last image.  Not the one that was just taken.
+static uint32_t last_capture_attempt = 0; // Time in milliseconds that the camera was attempted to be triggered.
 
 // Read all bytes from serial buffer.
 void clearSerialInputBuffer(void)
@@ -57,39 +62,55 @@ void clearSerialInputBuffer(void)
 // Trigger camera once command is received. Continuously called when device is connected and initialized.
 void CamStateHandlers::OnDeviceInitializedState(PTP *ptp)
 {
+    if (trigger_period > 0)
+    {
+        if ((millis() - last_capture_attempt) > trigger_period)
+        {
+            receivedTriggerCommand = true; // time to take a picture
+        }
+    }
+    
     if (!receivedTriggerCommand)
     {
-        return; // Don't want to take picture.    
+        return; // Don't want to take picture.
     }
     
     // Reset flag so we don't keep triggering.
     receivedTriggerCommand = false;
     
+    uint32_t capture_time = millis();
+    
     uint16_t ptp_return = eos.Capture();
     
-    // Adding a short delay seems to fix an issue where the camera would randomly revert to a 'not opened' state.
-    delay(200);
+    // Add a short delay before calling event dump below so camera can actually take image.
+    delay(300);
     
     // Clear any previous requests for triggering.  This allows client to send multiple requests at once to ensure one gets received.
-    clearSerialInputBuffer();
+    //clearSerialInputBuffer(); // KLM disabled because different types of commands are being sent now.
     
     if (ptp_return == PTP_RC_OK)
     {
         // Send back last image name (not the one we just took) and also a bunch of other data.
-        Serial.print(statusFrame);
+        Serial.print(statusStartFrame);
+        Serial.print("dump-");
         EOSEventDump hex;
         //eos.GetDeviceInfoEx(&hex);
         eos.EventCheck(&hex);
-        Serial.println(statusFrame);
+        Serial.println(statusEndFrame);
+        
+        // Send back elapsed time of previous image since that's what the filename in the event dump is for.
+        printTimeMessage(previous_capture_time - sync_time);
+        
+        // Update previous capture so can use it for next time.
+        previous_capture_time = capture_time;
     }
     else
     {
         // Send failure status report.
-        Serial.print(statusFrame);
-        Serial.print("Failure");
-        Serial.print(ptp_return, HEX);
-        Serial.println(statusFrame);
+        printStatusMessage("Trigger failure: " + String(ptp_return, HEX));
     }
+    
+    last_capture_attempt = capture_time;
 }
 
 // Send 'state' text back over serial port if we're trying to take a picture.  Should be called from non-triggering states.
@@ -97,10 +118,44 @@ void reportInWrongState(char const * state)
 {
     if (receivedTriggerCommand) 
     {
-        Serial.print(statusFrame);
-        Serial.print(state);
-        Serial.println(statusFrame);
+        printStatusMessage(state);
     }
+}
+
+// Send status message over serial port starting and stopping with frame.
+void printStatusMessage(String message)
+{
+    Serial.print(statusStartFrame);
+    Serial.print("status-" + message);
+    Serial.println(statusEndFrame);
+}
+
+// Overload for character array.
+void printStatusMessage(char const * message)
+{
+    printStatusMessage(String(message));
+}
+
+// Send time message over serial port starting and stopping with frame.
+void printTimeMessage(uint32_t time)
+{
+    Serial.print(statusStartFrame);
+    Serial.print("time-" + String(time));
+    Serial.println(statusEndFrame);
+}
+
+// Return new line terminated string from serial port. Newline not included.
+void readSerialLine(String & newString)
+{
+    while (Serial.available())
+    {
+        char inChar = (char)Serial.read(); 
+        if (inChar == '\n') 
+        {
+            return; // hit end of string.
+        }
+        newString += inChar;
+    } 
 }
 
 void setup()
@@ -110,7 +165,7 @@ void setup()
     if (usb.Init() == -1)
     {
         usbInitializedCorrectly = false;
-        Serial.println("OSC did not start.");
+        printStatusMessage("OSC did not start.");
     }
     
     clearSerialInputBuffer();
@@ -123,9 +178,7 @@ void loop()
     if (!usbInitializedCorrectly)
     {
         // Report USB failure status.
-        Serial.print(statusFrame);
-        Serial.print("USB Failure");
-        Serial.println(statusFrame);
+        printStatusMessage("USB Failure. Please reset.");
         delay(1000);
         return;
     }
@@ -134,16 +187,29 @@ void loop()
     {
         inByte = Serial.read();
         
-        if (inByte == 'a') // This is the special trigger character.
+        if (inByte == 's') // sync command
+        {
+            sync_time = millis();
+            if (synced_to_client)
+            {
+                printStatusMessage("Resynced");
+            }
+            synced_to_client = true;
+            Serial.print('a'); // acknowledge
+        }
+        else if (inByte == 't') // trigger command
         {
             receivedTriggerCommand = true;
         }
+        else if (inByte == 'p') // change periodic trigger command
+        {
+            String new_period;
+            readSerialLine(new_period);
+            trigger_period = new_period.toInt();
+        }
         else 
         {
-            Serial.print(statusFrame);
-            Serial.print("Invalid Byte: ");
-            Serial.print(inByte);
-            Serial.println(statusFrame);
+            printStatusMessage("Invalid Byte: " + inByte);
         }
     }
   
